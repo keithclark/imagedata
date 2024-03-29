ba44fb35c1725dd1287eaed92d2effd9dae57332
Keith Clark <keith@keithclark.co.uk>
2024-03-29 14:26:31 +0000

    Add STOS coder

---
+++ imagedata-coder-stos/README.md
@@ -0,0 +1,3 @@
+# STOS Packed Image to `ImageData` coder
+
+This module decodes a STOS memory bank containing packed screen data into an `ImageData` object so it can be rendered to canvas elements.
---
+++ imagedata-coder-stos/consts.js
@@ -0,0 +1,12 @@
+/** Magic string indicating the data is a STOS memory bank */
+export const MBK_FILE_HEADER_ID = 'Lionpoubnk';
+
+/** Length, in bytes, of the MBK file header */
+export const MBK_FILE_HEADER_LENGTH = 18;
+
+/** STOS data memory bank */
+export const MBK_TYPE_DATA = 0x81;
+
+/** Magic number to indicate the STOS data bank is a packed screen */
+export const MBK_DATA_TYPE_COMPRESSED_SCREEN = 0x06071963;
+
